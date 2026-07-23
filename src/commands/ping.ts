63214dee273b94bf43490bee513d118a13802f70
Jonathan Nicolas Gracia Heredia <nizolax.homehub@gmail.com>
2026-07-23 18:36:23 +0200

    fix: Remove stray .ts files and properly fix folder structure

---
+++ src/commands/ping.ts
@@ -0,0 +1,5 @@
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+
+export default new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with ping!');
---
+++ src/events/ready.ts
@@ -0,0 +1,3 @@
+import { Events } from 'discord.js';
+
+export default Events.ClientReady;
